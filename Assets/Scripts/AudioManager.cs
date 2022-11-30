e5c0c73b6be814d5d92e366b8ce7848fdbef1b9d
zivi80301 <tristan.zeller@unibe.ch>
2022-11-30 08:44:20 +0100

    added sample audio and UI button to play it

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    AudioSource TestSound;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        TestSound = GetComponent<AudioSource>();
+    }
+    public void OnPlayButton()
+    {
+        TestSound.Play();
+        print("Playing Sound");
     }
 }
